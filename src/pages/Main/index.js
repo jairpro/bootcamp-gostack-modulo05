43741ae12df3826c9dc4afdc5fe3c3dcec06cdb8
jairpro <jairedias@yahoo.com.br>
2020-02-23 08:30:17 -0300

    Removendo warning ao passar propriedade loading boolean
    Se em SubmitButton passar loading como bollean exibe:
    "Warning: Received `false` for a non-boolean attribute `loading`"

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -44,6 +44,8 @@ export default class Main extends Component {
   render() {
     const { newRepo, loading } = this.state;
 
+    // Se em SubmitButton passar loading como bollean exibe:
+    // "Warning: Received `false` for a non-boolean attribute `loading`"
     return (
       <Container>
         <h1>
@@ -59,7 +61,7 @@ export default class Main extends Component {
             onChange={this.handleInputChange}
           />
 
-          <SubmitButton loading={loading}>
+          <SubmitButton loading={loading ? 1 : 0}>
             {loading ? (
               <FaSpinner color="#FFF" size={14} />
             ) : (
