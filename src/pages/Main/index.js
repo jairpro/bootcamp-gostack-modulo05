24857bab60761806afdeb7c11a630bde2991bdff
jairpro <jairedias@yahoo.com.br>
2020-02-24 09:56:29 -0300

    Navegação de rotas

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,6 +3,7 @@
 /* eslint-disable react/state-in-constructor */
 import React, { Component } from "react";
 import { FaGithubAlt, FaPlus, FaSpinner } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 import api from "../../services/api";
 
@@ -101,7 +102,9 @@ export default class Main extends Component {
           {repositories.map(repository => (
             <li key={repository.name}>
               <span>{repository.name}</span>
-              <a href="">Detalhes</a>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
             </li>
           ))}
         </List>
