f0a189dd13566528f5ecb9d4a3e965b25d79a875
jairpro <jairedias@yahoo.com.br>
2020-02-24 13:50:24 -0300

    Definindo PropTypes

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,12 +1,23 @@
+/* eslint-disable react/state-in-constructor */
+/* eslint-disable react/static-property-placement */
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import api from "../../services/api";
 
 // import { Container } from './styles';
 
 export default class Repository extends Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string,
+      }),
+    }).isRequired,
+  };
+
   state = {
     repository: {},
-    issue: [],
+    issues: [],
     loading: true,
   };
 
